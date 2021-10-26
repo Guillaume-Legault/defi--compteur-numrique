4e4276b65a661e489e62540b8260b9fab70f186f
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-10-26 10:22:14 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    nb_personnes = nb_personnes + 1
+})
+input.onButtonPressed(Button.B, function () {
+    nb_personnes = nb_personnes - 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    nb_personnes = 20
+})
+input.onGesture(Gesture.Shake, function () {
+    nb_personnes = 0
+    basic.showString("Reset")
+})
+let nb_personnes = 0
+basic.clearScreen()
+nb_personnes = 0
+basic.forever(function () {
+    basic.showString("" + (nb_personnes))
+})
+basic.forever(function () {
+    if (nb_personnes == 20) {
+        music.ringTone(139)
+        basic.pause(100)
+        music.rest(music.beat(BeatFraction.Whole))
+        basic.showString("Max")
+    }
+})
